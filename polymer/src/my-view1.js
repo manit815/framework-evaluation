ad480363bc0784d21791e2f489f653291ee90389
Manit Kant <manit_k@HCL.COM>
2019-01-16 13:56:08 +0530

    creating form and test cases

--- polymer/src/my-view1.js
+++ polymer/src/my-view1.js
@@ -9,9 +9,26 @@
  */
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+// import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+
+import '@polymer/paper-radio-button/paper-radio-button.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-radio-group/paper-radio-group.js';
+import '@polymer/iron-form/iron-form.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+import { NeonAnimationRunnerBehavior } from '@polymer/neon-animation/neon-animation-runner-behavior.js';
+// import {IronOverlayBehavior} from '@polymer/iron-overlay-behavior/iron-overlay-behavior.js';
+// import '@polymer/neon-animation/animations/scale-down-animation.js';
+
 import './shared-styles.js';
 
-class MyView1 extends PolymerElement {
+class MyView1 extends mixinBehaviors([NeonAnimationRunnerBehavior], PolymerElement) {
   static get template() {
     return html`
       <style include="shared-styles">
@@ -23,12 +40,65 @@ class MyView1 extends PolymerElement {
       </style>
 
       <div class="card">
-        <div class="circle">1</div>
-        <h1>View One</h1>
-        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
-        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
+      <header>Account Opening Form</header>
+      <iron-form id="ingform">
+      <form>
+        <div>
+          <paper-radio-group selected="{{data.gender}}" value="{{data.gender}}">
+            <paper-radio-button name="male">Male</paper-radio-button>
+            <paper-radio-button name="female">Female</paper-radio-button>
+          </paper-radio-group>
+        </div>
+        <div>
+          <span>
+            <paper-input always-float-label label="First Name" value="{{data.firstName}}"></paper-input>
+            <paper-input always-float-label label="Last Name" value="{{data.lastName}}"></paper-input>
+          </span>
+        </div>
+        
+        <div>
+          <paper-dropdown-menu label="Nationality" value="{{data.nationality}}">
+            <paper-listbox slot="dropdown-content" selected="1">
+              <paper-item>India</paper-item>
+              <paper-item>Australia</paper-item>
+              <paper-item>Nethelands</paper-item>
+              <paper-item>Spain</paper-item>
+            </paper-listbox>
+          </paper-dropdown-menu>
+        </div>
+        <div>
+          <paper-checkbox checked={{data.tandc}}>Accept terms & conditions</paper-checkbox>
+        </div>
+        <div>
+          <paper-button raised class="indigo" on-click="submit">Submit</paper-button>
+        </div>
+      </div>
+      </form>
+      <div class="output"></div>
+      </iron-form>
+      <div>
+        <iron-ajax id="ajax" method="post" url="https://jsonplaceholder.typicode.com/posts" handle-as="json" last-response="{{ajaxResponse}}" on-response="handleRequest"
+      on-response="handleResponse" on-error="handleError"></iron-ajax>
+        </iron-ajax>
       </div>
     `;
+  }
+
+  static get properties() {
+    return {
+      data: {
+        type: Object,
+        value: {}
+      }
+    }
+  }
+
+  submit() {
+    console.log(this.data);
+    if (this.$.ingform.validate()) {
+      this.$.ajax.body=JSON.stringify(this.data);
+      this.$.ajax.generateRequest();
+    }
   }
 }
 
